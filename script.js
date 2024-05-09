725690d506fc8dc5d22d60fcdd4c878ba2e22b97
Keir Li <97122800+Keir-Li@users.noreply.github.com>
2024-05-09 18:58:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -71,22 +71,22 @@ function showContent(sectionId) {
     contents.forEach(content => content.classList.remove('active'));
     document.getElementById(sectionId + '-content').classList.add('active');
 
-    // Hide the animated text and finger when a button is clicked
-    document.querySelector('.animated-text').classList.add('text-hidden');
-    document.querySelector('.finger').classList.add('finger-hidden');
-    document.querySelector('.intro-text').classList.add('text-hidden');
-    document.querySelector('.intro-header').classList.add('text-hidden');   
-    document.querySelector('.intro-subheader').classList.add('text-hidden');   
-
-    // If the 'My Intro' button is clicked, reset the page to its initial state
+    // Show or hide the intro text and animated text based on which section is active
     if (sectionId === 'intro') {
         document.querySelector('.animated-text').classList.remove('text-hidden');
         document.querySelector('.finger').classList.remove('finger-hidden');
         document.querySelector('.intro-text').classList.remove('text-hidden');
         document.querySelector('.intro-header').classList.remove('text-hidden');   
         document.querySelector('.intro-subheader').classList.remove('text-hidden');   
+    } else {
+        document.querySelector('.animated-text').classList.add('text-hidden');
+        document.querySelector('.finger').classList.add('finger-hidden');
+        document.querySelector('.intro-text').classList.add('text-hidden');
+        document.querySelector('.intro-header').classList.add('text-hidden');   
+        document.querySelector('.intro-subheader').classList.add('text-hidden');   
     }
 }
+
 
 // Get the modal and the close button
 const modal = document.getElementById("fullDetailsModal");
